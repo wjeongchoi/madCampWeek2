73ce31e914ed775e7801cbc628a49a9452e4c684
wjeongchoi <artificialmint@gmail.com>
2024-01-05 01:43:28 +0900

    :pencil2: fix MyPageMain

--- app/App.js
+++ app/App.js
@@ -3,13 +3,11 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from '@expo/vector-icons'; // 아이콘 라이브러리 import
 
-// 여기에 각 탭에 해당하는 컴포넌트를 import 합니다.
 import SearchMain from "./Screens/SearchMain";
 import RecommMain from "./Screens/RecommMain";
 import Home from "./Screens/Home";
 import CommunityMain from "./Screens/CommunityMain";
-import myPageMain from "./Screens/myPageMain";
-// ... 나머지 탭 컴포넌트들도 import 합니다.
+import MyPageMain from "./Screens/MyPageMain";
 
 const Tab = createBottomTabNavigator();
 
@@ -59,7 +57,7 @@ export default function App() {
         />
         <Tab.Screen
           name="마이페이지"
-          component={myPageMain}
+          component={MyPageMain}
           options={{
             tabBarIcon: ({ color, size }) => (
               <Ionicons name="md-person-sharp" color={color} size={size} />
