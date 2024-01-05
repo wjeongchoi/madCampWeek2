8e3eed0c33526a49cfbb3ff2a03fcbdc53eb75bf
wjeongchoi <artificialmint@gmail.com>
2024-01-05 15:55:40 +0900

    :lipstick: apply text styles

--- app/Screens/CommunityMain.tsx
+++ app/Screens/CommunityMain.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
+import { text } from '../styles';
 
 export default function Home({  }) {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>커뮤니티</Text>
+      <Text style={[text.h1]}>커뮤니티</Text>
     </View>
   );
 }
