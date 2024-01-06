11c7091a7ac51afa9d7e955f811eede626793117
wjeongchoi <artificialmint@gmail.com>
2024-01-06 19:55:04 +0900

    :card_file_box: change db structure

--- back/models/models.py
+++ back/models/models.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from sqlalchemy import Column, Date, DateTime, ForeignKey, Integer, SmallInteger, String
+from sqlalchemy import Column, Date, DateTime, ForeignKey, Integer, SmallInteger, String, Table
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -10,8 +10,10 @@ metadata = Base.metadata
 class Cooker(Base):
     __tablename__ = 'cookers'
 
-    cookerName = Column(String(100), nullable=False, unique=True)
-    cookerID = Column(Integer, primary_key=True)
+    cookerName = Column(String(100), nullable=False)
+    cookerID = Column(String(36), primary_key=True)
+
+    users = relationship('User', secondary='myCookers')
 
 
 class DetailRecipe(Base):
@@ -24,16 +26,18 @@ class DetailRecipe(Base):
 
 
 class Ingredient(Base):
-    __tablename__ = 'ingredient'
+    __tablename__ = 'ingredients'
 
-    ingredientID = Column(Integer, primary_key=True)
+    ingredientID = Column(String(36), primary_key=True)
     ingredientName = Column(String(100), nullable=False)
+
+    users = relationship('User', secondary='myIngredients')
 
 
 class Recipe(Base):
     __tablename__ = 'recipes'
 
-    recipeID = Column(String(36), primary_key=True, unique=True)
+    recipeID = Column(String(36), primary_key=True)
     title = Column(String(100))
     subTitle = Column(String(300))
     manId = Column(Integer)
@@ -43,11 +47,14 @@ class Recipe(Base):
     modifiedTime = Column(Date)
     level = Column(SmallInteger)
 
+    users = relationship('User', secondary='likeRecipe')
+    users1 = relationship('User', secondary='myRecipe')
+
 
 class User(Base):
     __tablename__ = 'users'
 
-    userID = Column(Integer, primary_key=True)
+    userID = Column(String(36), primary_key=True)
     email = Column(String(50))
     password = Column(String(100))
     name = Column(String(50), nullable=False)
@@ -55,12 +62,41 @@ class User(Base):
     create_time = Column(DateTime, nullable=False)
 
 
-class IngredientCookerRecipe(Base):
-    __tablename__ = 'IngredientCookerRecipe'
+class CompleteRecipe(Base):
+    __tablename__ = 'completeRecipe'
 
-    recipeID = Column(String(36), primary_key=True, nullable=False)
-    ingredientID = Column(ForeignKey('ingredient.ingredientID'), primary_key=True, nullable=False, index=True)
+    recipeID = Column(ForeignKey('recipes.recipeID'), primary_key=True, nullable=False, index=True)
+    ingredientID = Column(ForeignKey('ingredients.ingredientID'), primary_key=True, nullable=False)
     cookerID = Column(ForeignKey('cookers.cookerID'), primary_key=True, nullable=False, index=True)
 
     cooker = relationship('Cooker')
     ingredient = relationship('Ingredient')
+    recipe = relationship('Recipe')
+
+
+t_likeRecipe = Table(
+    'likeRecipe', metadata,
+    Column('recipeID', ForeignKey('recipes.recipeID'), primary_key=True, nullable=False, index=True),
+    Column('userID', ForeignKey('users.userID'), primary_key=True, nullable=False)
+)
+
+
+t_myCookers = Table(
+    'myCookers', metadata,
+    Column('userID', ForeignKey('users.userID'), primary_key=True, nullable=False),
+    Column('cookerID', ForeignKey('cookers.cookerID'), primary_key=True, nullable=False, index=True)
+)
+
+
+t_myIngredients = Table(
+    'myIngredients', metadata,
+    Column('userID', ForeignKey('users.userID'), primary_key=True, nullable=False),
+    Column('ingredientID', ForeignKey('ingredients.ingredientID'), primary_key=True, nullable=False, index=True)
+)
+
+
+t_myRecipe = Table(
+    'myRecipe', metadata,
+    Column('recipeID', ForeignKey('recipes.recipeID'), primary_key=True, nullable=False, index=True),
+    Column('userID', ForeignKey('users.userID'), primary_key=True, nullable=False)
+)
