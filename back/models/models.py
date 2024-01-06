6b095296a1bdf689d7894b3fa98e4ceb47fb4f50
wjeongchoi <artificialmint@gmail.com>
2024-01-06 16:51:56 +0900

    :card_file_box: change db structure

--- back/models/models.py
+++ back/models/models.py
@@ -1,44 +1,66 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Enum, Date, UUID, SmallInteger
-from database import Base
+# coding: utf-8
+from sqlalchemy import Column, Date, DateTime, ForeignKey, Integer, SmallInteger, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+metadata = Base.metadata
+
+
+class Cooker(Base):
+    __tablename__ = 'cookers'
+
+    cookerName = Column(String(100), nullable=False, unique=True)
+    cookerID = Column(Integer, primary_key=True)
+
+
+class DetailRecipe(Base):
+    __tablename__ = 'detailRecipes'
+
+    detailRecipeID = Column(String(36), primary_key=True)
+    description = Column(String(300))
+    order = Column(Integer)
+    ImageSrc = Column(String(100))
+
+
+class Ingredient(Base):
+    __tablename__ = 'ingredient'
+
+    ingredientID = Column(Integer, primary_key=True)
+    ingredientName = Column(String(100), nullable=False)
+
+
+class Recipe(Base):
+    __tablename__ = 'recipes'
+
+    recipeID = Column(String(36), primary_key=True, unique=True)
+    title = Column(String(100))
+    subTitle = Column(String(300))
+    manId = Column(Integer)
+    cookTime = Column(SmallInteger)
+    like = Column(SmallInteger)
+    writedTime = Column(Date)
+    modifiedTime = Column(Date)
+    level = Column(SmallInteger)
+
 
 class User(Base):
-    __tablename__ = "users"
-    email = Column(String(50), primary_key=True, unique=True, index=True)
-    password = Column(String(100), nullable=True)
-    name = Column(String(50))
-    imageSrc =  Column(String(20), nullable=True)
-    # userType =  Column(Enum)
-    createdTime =  Column(Date, nullable=True)
-    deletedTime =  Column(Date, nullable=True)
-    
-    
-class Recipe(Base):
-    __tablename__ = "recipes"
-    recipeId = Column(UUID(as_uuid=True), primary_key=True, unique=True, index=True)
-    title = Column(String(100))
-    subTitle = Column(String(300), nullable=True)
-    manId =  Column(Integer, nullable=True)
-    cookTime =  Column(SmallInteger)
-    like =  Column(SmallInteger)
-    # userType =  Column(Enum)
-    writedTime =  Column(Date)
-    modifiedTime =  Column(Date)
-    level = Column(SmallInteger)
-    
-    
-class Ingradiant(Base):
-    __tablename__ = "ingradiant"
-    ingradiantName = Column(String(100), primary_key=True, unique=True)
-    
-    
-class Cooker(Base):
-    __tablename__ = "cookers"
-    cookerName = Column(String(100), primary_key=True, unique=True)
-    
-    
-class DetailRecipe(Base):
-    __tablename__ = "detailRecipes"
-    detailRecipeId =  Column(String(36), primary_key=True)
-    description = Column(String(300))
-    order = Column(Integer, autoincrement=True)
-    ImageSrc = Column(String(100), nullable=True)
+    __tablename__ = 'users'
+
+    userID = Column(Integer, primary_key=True)
+    email = Column(String(50))
+    password = Column(String(100))
+    name = Column(String(50), nullable=False)
+    imgSrc = Column(String(100))
+    create_time = Column(DateTime, nullable=False)
+
+
+class IngredientCookerRecipe(Base):
+    __tablename__ = 'IngredientCookerRecipe'
+
+    recipeID = Column(String(36), primary_key=True, nullable=False)
+    ingredientID = Column(ForeignKey('ingredient.ingredientID'), primary_key=True, nullable=False, index=True)
+    cookerID = Column(ForeignKey('cookers.cookerID'), primary_key=True, nullable=False, index=True)
+
+    cooker = relationship('Cooker')
+    ingredient = relationship('Ingredient')
