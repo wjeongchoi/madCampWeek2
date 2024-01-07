90a30560ad0afcbce51b70ee6d66b1958fd36d6b
wjeongchoi <artificialmint@gmail.com>
2024-01-08 03:44:34 +0900

    :construction: kakao login

--- app/screens/Intro.tsx
+++ app/screens/Intro.tsx
@@ -1,11 +1,28 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, Image, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, Image, StyleSheet, Linking } from 'react-native';
 import { text } from '../styles';
 import { RootStackScreenProps } from '../navigation/types';
+import { API_URL } from '@env';
 
 export const Intro: React.FC<RootStackScreenProps<"Intro">> = ({navigation}) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
+  const handleKakaoLogin = async () => {
+    try {
+      // This should be the endpoint that redirects to Kakao's login page
+      const kakaoLoginUrl = `${API_URL}/users/kakao`;
+      // Open the URL in the browser
+      const supported = await Linking.canOpenURL(kakaoLoginUrl);
+      if (supported) {
+        await Linking.openURL(kakaoLoginUrl);
+      } else {
+        console.error("Can't open Kakao login URL");
+      }
+    } catch (error) {
+      console.error("Kakao login error:", error);
+    }
+  };
 
   return (
     <View>
@@ -23,7 +40,7 @@ export const Intro: React.FC<RootStackScreenProps<"Intro">> = ({navigation}) => 
         onChangeText={setPassword}
       />
       <Button title="로그인" onPress={() => {/* 로그인 로직 */}} />
-      <Button title="카카오 로그인" onPress={() => {/* 카카오 로그인 로직 */}} />
+      <Button title="카카오 로그인" onPress={handleKakaoLogin}  />
       <Button title="회원가입" onPress={() => navigation.navigate("SignUp")}/>
     </View>
   );
