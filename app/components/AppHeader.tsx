8c0c1113f9ac6fb2c6bdb7d15d68ffdbf049dfd3
wjeongchoi <artificialmint@gmail.com>
2024-01-10 15:08:34 +0900

    :lipstick: header layout

--- app/components/AppHeader.tsx
+++ app/components/AppHeader.tsx
@@ -1,5 +1,5 @@
 import { Text, View } from "react-native";
-import { colors, text } from "../styles";
+import { colors, safe, text } from "../styles";
 
 type AppHeaderProps = {
   title: string; // 타이틀을 위한 props
@@ -10,7 +10,7 @@ export const AppHeader: React.FC<AppHeaderProps> = ({ title }) => {
     <View
       style={{ backgroundColor: colors.primary, height: 120, width: "100%", justifyContent:"center" }}
     >
-      <Text style={[text.h2, {paddingLeft: 24}, {paddingTop:50}, {color: colors.primaryDark}]}>{title}</Text>
+      <Text style={[text.h2, {paddingLeft: safe.horizontal}, {paddingTop:50}, {color: colors.primaryDark}]}>{title}</Text>
     </View>
   );
 };
